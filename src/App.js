1a4b580ace780f06ad46d8c5fad48736eb642bf5
tienngoc.uit@gmail.com <tienngoc.uit@gmail.com>
2023-03-30 11:31:11 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout https://www.youtube.com/watch?v=FwTQ0yoF0_g&list=PL_-VfJajZj0UXjlKfBwFX73usByw3Ph9Q&index=61

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+// import DefaultLayout from './components/Layout/DefaultLayout';
+import { DefaultLayout } from './components/Layout';
 function App() {
     return (
-        <div className="App">
-            <h1>REACT APP</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : route.layout || DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
