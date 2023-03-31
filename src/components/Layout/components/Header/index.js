48723152ce163b334bb3d560a65e74d46403cb18
tienngoc.uit@gmail.com <tienngoc.uit@gmail.com>
2023-03-31 13:43:38 +0700

    Xây Dựng UI Phần Header https://www.youtube.com/watch?v=hFK4okw-XYs&list=PL_-VfJajZj0UXjlKfBwFX73usByw3Ph9Q&index=63

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,29 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/asset/images';
 
 const cx = classNames.bind(styles);
-
+console.log(images.logo);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* search */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="image" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
